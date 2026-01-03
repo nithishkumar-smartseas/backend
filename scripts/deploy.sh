9a0f54b927e0b8634990f2216e3a9c07776b8697
hello-world <hello-world@gmail.com>
2026-01-03 22:44:18 +0530

    deployment files added

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Logging in to ECR"
+aws ecr get-login-password --region us-east-1 \
+| docker login --username AWS --password-stdin 991940085316.dkr.ecr.us-east-1.amazonaws.com
+
+echo "Stopping old container (if exists)"
+docker stop backend || true
+docker rm backend || true
+
+echo "Pulling new image"
+docker pull 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+
+echo "Starting container"
+docker run -d -p 80:4000 --name backend 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
