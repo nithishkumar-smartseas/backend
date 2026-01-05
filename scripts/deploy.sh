9484bf3fdd214266576a2b8e55d86d611a35fe7d
hello-world <hello-world@gmail.com>
2026-01-05 16:48:38 +0530

    deploy files updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,5 +12,5 @@ docker rm backend || true
 echo "Pulling new image"
 docker pull 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
 
-echo "Starting container"
-docker run -d -p 4000:4000 --name backend 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+docker run -d --name backend --network app-network 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+
