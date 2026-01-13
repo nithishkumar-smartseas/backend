6695c2b88699917d202d4f167d65a8e4fc81701d
hello-world <hello-world@gmail.com>
2026-01-13 16:34:09 +0530

    localhost

--- tracing.js
+++ tracing.js
@@ -4,6 +4,9 @@ const { trace } = require("@opentelemetry/api");
 
 const sdk = new NodeSDK({
   serviceName: "backend",
+  traceExporter: new OTLPTraceExporter({
+    url: "http://172.17.0.1:4318/v1/traces"
+  }),
   instrumentations: [new HttpInstrumentation()],
 });
 
