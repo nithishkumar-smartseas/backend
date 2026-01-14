26c0bb4b84661d5f1804be2427108d66a146c380
hello-world <hello-world@gmail.com>
2026-01-14 08:43:03 +0530

    traces

--- tracing.js
+++ tracing.js
@@ -8,9 +8,6 @@ const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "backend",
   }),
-  traceExporter: new OTLPTraceExporter({
-    url: "http://172.17.0.1:4318/v1/traces",
-  }),
   instrumentations: [new HttpInstrumentation()],
 });
 
