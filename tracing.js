bb46d1e3e69357a9f90959166f40e8f04aec2a26
hello-world <hello-world@gmail.com>
2026-01-14 08:50:17 +0530

    opentelementary updated

--- tracing.js
+++ tracing.js
@@ -1,13 +1,9 @@
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { trace } = require("@opentelemetry/api");
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "backend",
-  }),
+  serviceName: "backend",
   instrumentations: [new HttpInstrumentation()],
 });
 
