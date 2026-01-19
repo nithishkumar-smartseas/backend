7884fb7bdb46d42e660498476ce76d8e8251b1c6
hello-world <hello-world@gmail.com>
2026-01-19 08:45:33 +0530

    backend

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ docker stop backend || true
 docker rm -f backend || true
 
 echo "Pulling new image"
-docker pull 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+docker pull 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest 
 
 docker run -d --name backend --network app-network --restart unless-stopped 991940085316.dkr.ecr.us-east-1.amazonaws.com/backend:latest
 
